76de113804c3c12262f7a8807bd58e4d5efed3ba
ErikSyan <erik-sargsyan-2004@bk.ru>
2024-06-20 01:46:01 +0400

    Описание изменений

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <Header />
+      <Nav/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react'
+
+
+export default class Nav extends Component {
+  render() {
+
+    
+   
+
+    return (
+
+      
+      
+      <div className='rumb'>
+        <img
+        
+          className='nk'></img>
+        Iskzbane mtadrutyun e exel unenal Морской бой (Sea Battle) xaxy bayc npatakaharmar er stanal minesweeper xaxy
+        arajikayum klini nor barelavummner xaxum
+
+
+        <div className='xaxiMasin'>Rumbnery 21 hat en ete gtnes dranq 55 porcic uremn du haxtel es isk rumbnery gtnelu hamar uxxaki sexmir ayn vandaki vra vory karcum es gaxtniq uni BARI XAX   </div>
+      </div>
+
+        
+    )
+  }
+}
+
+
+
+
+
+// export default class Nav extends Component {
+//   render() {
+
+    
+
+//     return
+//   }
+// }
+
+
+
+
+
+
+// import React, { Component, useState } from 'react';
+// import { GiBladeBite } from 'react-icons/gi';
+
+// let navv = document.getElementById("navv")
+
+
+// let [isActive,setActive] = useState(false)
+
+// export default class Nav extends Component {
+//    handleClick = () => {
+//     setActive(!isActive)
+//   };
+
+//   render() {
+//     return (
+//       <div >  
+        
+
+
+
+        
+//       </div>
+//     );
+//   }
+// }
